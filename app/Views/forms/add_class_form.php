50efec4ef1381c5d12e81f788bb1554e41800fcb
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-02-26 21:53:13 +0530

    Add class form created
    Co-Authored-By: waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>

---
+++ app/Views/forms/add_class_form.php
@@ -0,0 +1,60 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Add Class Form</h1>
+
+<form class="user">
+<div class="form-group row">
+            <div class="col-sm-4 mb-3 mb-sm-0">
+            <label><strong>Class ID</strong></label><br>
+                <input type="text" class="form-control" id="subject_id" placeholder="Class ID" name="subject_id">
+            </div>
+            <div class="col-sm-4">
+            <label><strong>Teacher ID</strong> </label><br>
+                <input type="text" class="form-control" id="teacher_id" placeholder="Teacher ID" name="teacher_id">
+            </div>
+            <div class="col-sm-4">
+            <label><strong>Class Name</strong> </label><br>
+                <input type="text" class="form-control" id="class_name" placeholder="Class Name" name="class_name">
+            </div>
+        </div>
+<hr>
+
+<div class="form-group row">
+            <div class="col-sm-4 mb-3 mb-sm-0">
+            <label><strong>Number Of Students</strong></label><br>
+                <input type="text" class="form-control" id="subject_id" placeholder="Class ID" name="subject_id">
+            </div>
+            
+            <div class="col-sm-4">
+            <label><strong>Year</strong> </label><br>
+                    <div class="input-group mb-3">
+                        <select class="custom-select" id="medium_id" name="medium_id">
+                            <option selected>Year</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                        </select> 
+                    </div>
+            </div>
+
+            <hr>
+        <br><br><br><br>
+        <a href=".html" class="btn btn-primary btn-block">
+        Add Class
+        </a>
+</form>
+
+
+
