9bb8635a529371afa0dfc2b30cecefc2eacbcf3f
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-02-26 21:07:23 +0530

    Add grama niladhari division form
    Co-Authored-By: waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>

---
+++ app/Views/forms/add_gramaniladhari_division_form.php
@@ -0,0 +1,22 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Grama Niladhari Division Form</h1>
+
+<form class="user">
+<div class="form-group row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+            <label><strong>Grama Niladhari Division ID</strong></label><br>
+                <input type="text" class="form-control" id="gnd_id" placeholder="Grama Niladhari Division ID" name="gnd_id">
+            </div>
+            <div class="col-sm-6">
+            <label><strong>Grama Niladhari Division Name</strong> </label><br>
+                <input type="text" class="form-control" id="gnd_name" placeholder="Grama Niladhari Division name" name="gnd_name">
+            </div>
+        </div>
+        <br>
+        <a href=".html" class="btn btn-primary btn-block">
+            Add Grama Niladhari Division
+        </a>
+</form>
