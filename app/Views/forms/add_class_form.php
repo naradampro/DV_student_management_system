6322a77d65ff6ddadd86cd187f9f18cf28926e8d
Jayoda Halloluwa <jshalloluwa@students.nsbm.ac.lk>
2022-03-03 09:48:06 +0530

    Forms modified.
    Co-Authored-By: Dilki Ayodhya <68230139+Ayodhya12@users.noreply.github.com>

--- app/Views/forms/add_class_form.php
+++ app/Views/forms/add_class_form.php
@@ -5,55 +5,48 @@
 <h1 class="h3 mb-4 text-gray-800">Add Class Form</h1>
 
 <form class="user">
-<div class="form-group row">
-            <div class="col-sm-4 mb-3 mb-sm-0">
-            <label><strong>Class ID</strong></label><br>
-                <input type="text" class="form-control" id="subject_id" placeholder="Class ID" name="subject_id">
-            </div>
-            <div class="col-sm-4">
-            <label><strong>Teacher ID</strong> </label><br>
-                <input type="text" class="form-control" id="teacher_id" placeholder="Teacher ID" name="teacher_id">
-            </div>
-            <div class="col-sm-4">
-            <label><strong>Class Name</strong> </label><br>
-                <input type="text" class="form-control" id="class_name" placeholder="Class Name" name="class_name">
+    <div class="form-group row">
+        <!--Class Name-->
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <label>
+                <strong>Class Name</strong>
+            </label><br>
+            <input type="text" class="form-control" id="class_name" placeholder="Class Name" name="class_name">
+        </div>
+         <!--Teacher Name-->
+        <div class="col-sm-6">
+            <label>
+                <strong>Teacher Name</strong> 
+            </label><br>
+            <div class="input-group mb-3">
+                <select class="custom-select" id="teacher_id" name="teacher_id">
+                    <option selected>Choose Teacher</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select> 
             </div>
         </div>
-<hr>
+    </div>
 
-<div class="form-group row">
-            <div class="col-sm-4 mb-3 mb-sm-0">
-            <label><strong>Number Of Students</strong></label><br>
-                <input type="text" class="form-control" id="subject_id" placeholder="Class ID" name="subject_id">
-            </div>
-            
-            <div class="col-sm-4">
-            <label><strong>Year</strong> </label><br>
-                    <div class="input-group mb-3">
-                        <select class="custom-select" id="medium_id" name="medium_id">
-                            <option selected>Year</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                            <option value="13">13</option>
-                        </select> 
-                    </div>
-            </div>
+    <div class="form-group row">
+         <!--Number Of Students-->
+        <div class="col-sm-6 mb-3 mb-sm-0">
+            <label>
+                <strong>Number Of Students</strong>
+            </label><br>
+            <input type="text" class="form-control" id="number_of_students" placeholder="Number Of Students" name="number_of_students">
+        </div>  
+        <!--Year-->
+        <div class="col-sm-6">
+            <label>
+                <strong>Year</strong> 
+            </label><br>
+            <input type="date" class="form-control" id="year" name="year">
+        </div>
+    </div>
 
-            <hr>
-        <br><br><br><br>
-        <a href=".html" class="btn btn-primary btn-block">
-        Add Class
-        </a>
+    <input type="submit" name="submit"  class="btn btn-primary btn-block" value="Add Class" />
 </form>
 
 
